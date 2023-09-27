140670ad3661aba20083bbe61d7cb989cfd8ea4a
lirismankarina <lirisman.karina@huawei-partners.com>
2023-09-27 23:56:03 +0300

    [Additional][Nested] Completed

--- additional_nested_omp/README.md
+++ additional_nested_omp/README.md
@@ -4,3 +4,42 @@
 
 ### Схема работы вложенности
 <image src="nesting.png">
+
+В стандарте OpenMP для выяснения своих координат в дереве потоков есть:
+
+1. **omp_get_thread_num()**: Номер текущего потока в рамках текущего уровня параллелизма.
+
+```
+int my_thread_num = omp_get_thread_num();
+```
+
+2. **omp_get_num_threads()**: С помощью этой функции можно получить общее количество потоков, работающих на текущем уровне параллелизма.
+
+```
+int num_threads = omp_get_num_threads();
+```
+
+3. **omp_get_level()**: Текущий уровень вложенности параллелизма.
+
+```
+int current_level = omp_get_level();
+```
+
+4. **omp_get_ancestor_thread_num()**: Номер потока на более высоком уровне вложенности.
+
+```
+int ancestor_thread_num = omp_get_ancestor_thread_num(level);
+```
+
+5. **omp_get_team_size()**: Количество потоков в текущей команде выполнения, которая может быть меньше общего количества потоков на уровне.
+
+```
+int team_size = omp_get_team_size(level);
+```
+
+### Скриншоты работы
+<image src="1.png">
+
+<image src="2.png">
+
+<image src="3.png">
--- additional_nested_omp/additional_nested_omp.cpp
+++ additional_nested_omp/additional_nested_omp.cpp
@@ -15,48 +15,44 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // должно отпачковываться, и на каждом уровне количество потоков суммарно есть прошлое умноженное на нынешнее
-
+    if (std::atoi(argv[1]) > omp_get_thread_limit() || std::atoi(argv[1]) < 3) {
+        std::cout << "Invalid arguments. Enter number of threads in range [3, 2147483647]." << std::endl;
+        return 2;
+    }
     int threads_from_user = std::atoi(argv[1]);
 
-    const int level_1 = 5;
-    const int level_2 = 3;
-    const int level_3 = 2;
+    const int level_1 = threads_from_user - 1;
+    const int level_2 = threads_from_user - 2;
+    const int level_3 = threads_from_user - 3;
 
-    int counter = 0;
-
-    // why only 8 left?? but more than 8 left when nesting is off
-    #pragma omp parallel shared(counter) num_threads(level_1)
+    #pragma omp parallel num_threads(level_1)
     {
         int thread_num_1 = omp_get_thread_num(); // using omp funcs
         int num_threads_1 = omp_get_num_threads(); // Returns the number of threads in the team currently executing the parallel region from which it is called
         #pragma omp critical // Using for preventing conflicts in out stream
         {
-            counter +=1;
             std::cout << "Thread num = " << thread_num_1
                       << ". Summ of threads on this level = " << num_threads_1
                       << ". Level of nesting = " << omp_get_level() << std::endl;
         }
-        #pragma omp parallel shared(counter, thread_num_1, num_threads_1)
+        #pragma omp parallel shared(thread_num_1, num_threads_1) num_threads(level_2)
         {
             int thread_num_2 = omp_get_thread_num() + omp_get_num_threads() * thread_num_1; // using omp funcs
             int num_threads_2 = omp_get_num_threads() * num_threads_1; // Returns the number of threads in the team currently executing the parallel region from which it is called
             #pragma omp critical // Using for prevent conflicts in out stream
             {
-                counter += 1;
                 std::cout << "Thread num = " << thread_num_2
                           << ". Summ of threads on this level = " << num_threads_2 
                           << ". Summ of threads on the previous level = " << num_threads_1
                           << ". Level of nesting = " << omp_get_level() << std::endl;
             }
 
-            #pragma omp parallel shared(counter, thread_num_2, num_threads_2)
+            #pragma omp parallel shared(thread_num_2, num_threads_2) num_threads(level_3)
             {
                 int thread_num_3 = omp_get_thread_num() + omp_get_num_threads() * thread_num_2; // using omp funcs
                 int num_threads_3 = omp_get_num_threads() * num_threads_2; // Returns the number of threads in the team currently executing the parallel region from which it is called
                 #pragma omp critical // Using for prevent conflicts in out stream
                 {
-                    counter += 1;
                     std::cout << "Thread num = " << thread_num_3
                               << ". Summ of threads on this level = " << num_threads_3
                               << ". Summ of threads on the previous level = " << num_threads_1
