c302837a19c5017b27c8eebbb2cc309fa396d013
lirismankarina <lirisman.karina@huawei-partners.com>
2023-10-02 18:04:27 +0300

    [Additional][Matrix] Multiplication added

--- additional_matrix/additional_matrix.cpp
+++ additional_matrix/additional_matrix.cpp
@@ -14,8 +14,9 @@
         }
     }
 */
+using matrix_type = std::vector<std::vector<int>>;
 
-void fill_matrix(std::vector<std::vector<int>>& matrix, const int &N) {
+void fill_matrix(matrix_type& matrix, const int &N) {
 
     const int min_value = 1;
     const int max_value = 99;
@@ -31,7 +32,7 @@ void fill_matrix(std::vector<std::vector<int>>& matrix, const int &N) {
     }
 }
 
-void print_matrix(std::vector<std::vector<int>>& matrix, const int &N, const std::string &msg) {
+void print_matrix(matrix_type& matrix, const int &N, const std::string &msg) {
 
     const int max_width = 5;
 
@@ -45,6 +46,39 @@ void print_matrix(std::vector<std::vector<int>>& matrix, const int &N, const std
     }
 
     std::cout << "-----------------------" << std::endl;
+}
+
+matrix_type matrix_multiplication_sequential(const matrix_type& matrixA, const matrix_type& matrixB) {
+
+    const int N = matrixA.size();
+
+    matrix_type matrixRes(N, std::vector<int>(N, 0));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                matrixRes[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+
+    return matrixRes;
+}
+
+matrix_type matrix_multiplication_parallel(const matrix_type& matrixA, const matrix_type& matrixB) {
+
+    const int N = matrixA.size();
+
+    matrix_type matrixRes(N, std::vector<int>(N, 0));
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                matrixRes[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+
+    return matrixRes;
 }
  
 
@@ -62,14 +96,21 @@ int main(int argc, char *argv[]) {
     const int N = std::atoi(argv[1]); // Размер квадратной матрицы
 
     // Создание двух квадратных матриц A и B
-    std::vector<std::vector<int>> matrixA(N, std::vector<int>(N, 0));
-    std::vector<std::vector<int>> matrixB(N, std::vector<int>(N, 0));
+    matrix_type matrixA(N, std::vector<int>(N, 0));
+    matrix_type matrixB(N, std::vector<int>(N, 0));
 
     fill_matrix(matrixA, N);
     fill_matrix(matrixB, N);
 
     print_matrix(matrixA, N, "MATRIX A");
     print_matrix(matrixB, N, "MATRIX B");
+
+    matrix_type matrixResultSeq = matrix_multiplication_sequential(matrixA, matrixB);
+    print_matrix(matrixResultSeq, N, "RESULT MATRIX - SEQUENTIAL MODE");
+
+    matrix_type matrixResultPar = matrix_multiplication_parallel(matrixA, matrixB);
+    print_matrix(matrixResultPar, N, "RESULT MATRIX - PARALLEL MODE");
+
 
     return 0;
 }
