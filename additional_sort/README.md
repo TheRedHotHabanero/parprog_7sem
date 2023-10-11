da74402c8191030676577d4aeedf24bb9d27c4b0
lirismankarina <lirisman.karina@huawei-partners.com>
2023-10-12 00:21:38 +0300

    [Additional][Sort] Added full graphics

--- additional_sort/README.md
+++ additional_sort/README.md
@@ -2,9 +2,19 @@
 ## Запуск
 [См главную ридмишку](../README.md)
 
-### Скриншоты работы
+### Сравнение ускорений
+
+100
 <image src="screenshots/1.png">
 
+200
 <image src="screenshots/2.png">
 
+50
 <image src="screenshots/3.png">
+
+300
+<image src="screenshots/4.png">
+
+Fast version for all tresholds
+<image src="screenshots/fast_for_all.png">
--- additional_sort/plot.py
+++ additional_sort/plot.py
@@ -4,7 +4,7 @@ with open('resultss.txt', 'r') as f:
     #fig = plt.figure()
     xs  = []
     y1s = []
-    y2s = []
+    y1fast = []
     line = f.readline()
     line = f.readline()
     line = f.readline()
@@ -15,13 +15,13 @@ with open('resultss.txt', 'r') as f:
         line = line.split(',')
         xs.append(float(line[0]))
         y1s.append(float(line[1]))
-        y2s.append(float(line[2][:-1]))
+        y1fast.append(float(line[2][:-1]))
 
     plt.title("2 vs 1 Variant. treshold = 100")
     plt.xlabel("matrix size")
     plt.ylabel("time, msecs")
     plt.scatter(xs, y1s, label="Slow")
-    plt.scatter(xs, y2s, label="Fast")
+    plt.scatter(xs, y1fast, label="Fast")
     plt.legend()
     plt.show()
 
@@ -30,7 +30,7 @@ with open('resultss.txt', 'r') as f:
 
     xs  = []
     y1s = []
-    y2s = []
+    y2fast = []
 
     while True:
         line = f.readline()
@@ -39,13 +39,13 @@ with open('resultss.txt', 'r') as f:
         line = line.split(',')
         xs.append(float(line[0]))
         y1s.append(float(line[1]))
-        y2s.append(float(line[2][:-1]))
+        y2fast.append(float(line[2][:-1]))
 
     plt.title("2 vs 1 Variant. treshold = 200")
     plt.xlabel("matrix size")
     plt.ylabel("time, msecs")
     plt.scatter(xs, y1s, label="Slow")
-    plt.scatter(xs, y2s, label="Fast")
+    plt.scatter(xs, y2fast, label="Fast")
     plt.legend()
     plt.show()
 
@@ -54,7 +54,7 @@ with open('resultss.txt', 'r') as f:
 
     xs  = []
     y1s = []
-    y2s = []
+    y3fast = []
 
     while True:
         line = f.readline()
@@ -63,14 +63,14 @@ with open('resultss.txt', 'r') as f:
         line = line.split(',')
         xs.append(float(line[0]))
         y1s.append(float(line[1]))
-        y2s.append(float(line[2][:-1]))
+        y3fast.append(float(line[2][:-1]))
 
     plt.title("2 vs 1 Variant. treshold = 50")
     plt.xlabel("matrix size")
     plt.ylabel("time, msecs")
-    plt.legend()
     plt.scatter(xs, y1s, label="Slow")
-    plt.scatter(xs, y2s, label="Fast")
+    plt.scatter(xs, y3fast, label="Fast")
+    plt.legend()
     plt.show()
 
     line = f.readline()
@@ -78,7 +78,7 @@ with open('resultss.txt', 'r') as f:
 
     xs  = []
     y1s = []
-    y2s = []
+    y4fast = []
 
     while True:
         line = f.readline()
@@ -87,12 +87,22 @@ with open('resultss.txt', 'r') as f:
         line = line.split(',')
         xs.append(float(line[0]))
         y1s.append(float(line[1]))
-        y2s.append(float(line[2][:-1]))
+        y4fast.append(float(line[2][:-1]))
 
     plt.title("2 vs 1 Variant. treshold = 300")
     plt.xlabel("matrix size")
     plt.ylabel("time, msecs")
-    plt.legend()
     plt.scatter(xs, y1s, label="Slow")
-    plt.scatter(xs, y2s, label="Fast")
+    plt.scatter(xs, y4fast, label="Fast")
+    plt.legend()
+    plt.show()
+
+    plt.title("Fast variant for all treshold")
+    plt.xlabel("matrix size")
+    plt.ylabel("time, msecs")
+    plt.scatter(xs, y1fast, label="100")
+    plt.scatter(xs, y2fast, label="200")
+    plt.scatter(xs, y3fast, label="50")
+    plt.scatter(xs, y4fast, label="300")
+    plt.legend()
     plt.show()
